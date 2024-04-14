ae0de71ea40acb409ff6d75eb797e1956276178f
Eric Kampman <erickampman@yahoo.com>
2024-04-14 12:50:04 -0700

    Initial Commit

---
+++ Singleton1/Singleton1App.swift
@@ -0,0 +1,17 @@
+//
+//  Singleton1App.swift
+//  Singleton1
+//
+//  Created by Eric Kampman on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Singleton1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Singleton1UITests/Singleton1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Singleton1UITestsLaunchTests.swift
+//  Singleton1UITests
+//
+//  Created by Eric Kampman on 4/14/24.
+//
+
+import XCTest
+
+final class Singleton1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
